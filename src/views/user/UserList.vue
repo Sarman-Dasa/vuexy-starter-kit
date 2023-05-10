559c7ee48d855db932570c02d69702157994bd66
Sarman-Dasa <sarmand@zignuts.com>
2023-05-10 14:25:17 +0530

    user listing, seraching, filtering and pagination

---
+++ src/views/user/UserList.vue
@@ -0,0 +1,214 @@
+<template>
+  <b-row>
+    <b-col md="2" sm="4" class="my-1">
+      <b-form-group class="mb-0">
+        <label class="d-inline-block text-sm-left mr-50">Per page</label>
+        <b-form-select
+          id="perPageSelect"
+          v-model="perPage"
+          size="sm"
+          :options="pageOptions"
+          class="w-50"
+        />
+      </b-form-group>
+    </b-col>
+    <b-col md="4" sm="8" class="my-1">
+      <b-form-group
+        label="Sort"
+        label-cols-sm="3"
+        label-align-sm="right"
+        label-size="sm"
+        label-for="sortBySelect"
+        class="mb-0"
+      >
+        <b-input-group size="sm">
+          <b-form-select
+            id="sortBySelect"
+            v-model="sortBy"
+            :options="sortOptions"
+            class="w-75"
+          >
+            <template v-slot:first>
+              <option value="">-- none --</option>
+            </template>
+          </b-form-select>
+          <b-form-select
+            v-model="sortDesc"
+            size="sm"
+            :disabled="!sortBy"
+            class="w-25"
+          >
+            <option :value="false">Asc</option>
+            <option :value="true">Desc</option>
+          </b-form-select>
+        </b-input-group>
+      </b-form-group>
+    </b-col>
+    <b-col md="6" class="my-1">
+      <b-form-group
+        label="Filter"
+        label-cols-sm="3"
+        label-align-sm="right"
+        label-size="sm"
+        label-for="filterInput"
+        class="mb-0"
+      >
+        <b-input-group size="sm">
+          <b-form-input
+            id="filterInput"
+            v-model="filter"
+            type="search"
+            placeholder="Type to Search"
+          />
+          <b-input-group-append>
+            <b-button :disabled="!filter" @click="filter = ''">
+              Clear
+            </b-button>
+          </b-input-group-append>
+        </b-input-group>
+      </b-form-group>
+    </b-col>
+
+    <b-col cols="12">
+      <b-table
+        striped
+        hover
+        responsive
+        :per-page="perPage"
+        :current-page="currentPage"
+        :items="items"
+        :fields="fields"
+        :sort-by.sync="sortBy"
+        :sort-desc.sync="sortDesc"
+        :sort-direction="sortDirection"
+        :filter="filter"
+        :filter-included-fields="filterOn"
+        @filtered="onFiltered"
+      >
+        <template #cell(is_active)="data">
+          <b-badge :variant="is_active[1][data.value]">
+            {{ is_active[0][data.value] }}
+          </b-badge>
+        </template>
+        
+        <template #cell(phone)="data">
+            <span class="text-info">
+                {{ data.value }}
+            </span>
+        </template>
+
+        <template #cell(email)="data">
+            <span class="text-primary">
+                <a href = "mailto: abc@example.com">Send Email</a>
+            </span>
+        </template>
+      </b-table>
+    </b-col>
+
+    <b-col cols="12">
+      <b-pagination
+        v-model="currentPage"
+        :total-rows="totalRows"
+        :per-page="perPage"
+        align="center"
+        size="sm"
+        class="my-0"
+      />
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+import {
+  BTable,
+  BAvatar,
+  BBadge,
+  BRow,
+  BCol,
+  BFormGroup,
+  BFormSelect,
+  BPagination,
+  BInputGroup,
+  BFormInput,
+  BInputGroupAppend,
+  BButton,
+} from "bootstrap-vue";
+import axios from "axios";
+export default {
+  components: {
+    BTable,
+    BAvatar,
+    BBadge,
+    BRow,
+    BCol,
+    BFormGroup,
+    BFormSelect,
+    BPagination,
+    BInputGroup,
+    BFormInput,
+    BInputGroupAppend,
+    BButton,
+  },
+  data() {
+    return {
+      perPage: 5,
+      pageOptions: [5, 10, 25],
+      totalRows: 1,
+      currentPage: 1,
+      sortBy: "",
+      sortDesc: false,
+      sortDirection: "asc",
+      filter: null,
+      filterOn: [],
+      fields: [
+        {
+          key: "id",
+          label: "Id",
+        },
+        { key: "first_name", label: "First Name", sortable: true },
+        { key: "last_name", label: "Last Name", sortable: true },
+        { key: "phone", label: "Phone", sortable: true },
+        { key: "email", label: "Email", sortable: true },
+        { key: "is_active", label: "Status", sortable: true },
+      ],
+      items: [],
+      is_active: [
+        {
+          true: "Active",
+          false: "Deactive",
+        },
+        {
+          true: "light-success",
+          0: "light-primary",
+        },
+      ],
+    };
+  },
+  computed: {
+    sortOptions() {
+      // Create an options list from our fields
+      return this.fields
+        .filter((f) => f.sortable)
+        .map((f) => ({ text: f.label, value: f.key }));
+    },
+  },
+  async mounted() {
+    this.token = JSON.parse(localStorage.getItem("userData")).accessToken;
+    console.log("Token :", this.token);
+    await axios
+      .post("list", {}, { headers: { Authorization: `Bearer ${this.token}` } })
+      .then((success) => {
+        this.items = success.data.data.users;
+      });
+    // Set the initial number of items
+    this.totalRows = this.items.length;
+  },
+  methods: {
+    onFiltered(filteredItems) {
+      // Trigger pagination to update the number of buttons/pages due to filtering
+      this.totalRows = filteredItems.length;
+      this.currentPage = 1;
+    },
+  },
+};
+</script>
