d4ec5779d892c7bf4be2c72fe8d8212fe5726880
Sarman-Dasa <sarmand@zignuts.com>
2023-05-17 19:47:56 +0530

    change table style to responsive design

--- src/views/user/UserList.vue
+++ src/views/user/UserList.vue
@@ -1,121 +1,129 @@
 <template>
-  <b-row>
-    <b-col md="2" sm="4" class="my-1">
-      <b-form-group class="mb-0">
-        <label class="d-inline-block text-sm-left mr-50">Per page</label>
-        <b-form-select
-          id="perPageSelect"
-          v-model="perPage"
-          size="sm"
-          :options="pageOptions"
-          class="w-50"
-        />
-      </b-form-group>
-    </b-col>
-    <b-col md="4" sm="8" class="my-1">
-      <b-form-group
-        label="Sort"
-        label-cols-sm="3"
-        label-align-sm="right"
-        label-size="sm"
-        label-for="sortBySelect"
-        class="mb-0"
-      >
-        <b-input-group size="sm">
+  <div>
+    <b-row>
+      <b-col md="2" sm="2" class="my-1">
+        <b-form-group class="mb-0"
+        label-cols-sm="4"
+        label="Per page"
+        >
+          <!-- <label 
+          class="d-inline-block text-sm-left mr-50">Per page</label> -->
           <b-form-select
-            id="sortBySelect"
-            v-model="sortBy"
-            :options="sortOptions"
-            class="w-75"
-          >
-            <template v-slot:first>
-              <option value="">-- none --</option>
-            </template>
-          </b-form-select>
-          <b-form-select
-            v-model="sortDesc"
+            id="perPageSelect"
+            v-model="perPage"
             size="sm"
-            :disabled="!sortBy"
-            class="w-25"
-          >
-            <option :value="false">Asc</option>
-            <option :value="true">Desc</option>
-          </b-form-select>
-        </b-input-group>
-      </b-form-group>
-    </b-col>
-    <b-col md="6" class="my-1">
-      <b-form-group
-        label="Filter"
-        label-cols-sm="3"
-        label-align-sm="right"
-        label-size="sm"
-        label-for="filterInput"
-        class="mb-0"
-      >
-        <b-input-group size="sm">
-          <b-form-input
-            id="filterInput"
-            v-model="filter"
-            type="search"
-            placeholder="Type to Search"
+            :options="pageOptions"
+            class="w-100"
           />
-          <b-input-group-append>
-            <b-button :disabled="!filter" @click="filter = ''">
-              Clear
-            </b-button>
-          </b-input-group-append>
-        </b-input-group>
-      </b-form-group>
-    </b-col>
+        </b-form-group>
+      </b-col>
+      <b-col md="4" sm="4" class="my-1">
+        <b-form-group
+          label="Sort"
+          label-cols-sm="3"
+          label-align-sm="right"
+          label-size="sm"
+          label-for="sortBySelect"
+          class="mb-0"
+        >
+          <b-input-group size="sm">
+            <b-form-select
+              id="sortBySelect"
+              v-model="sortBy"
+              :options="sortOptions"
+              class="w-75"
+            >
+              <template v-slot:first>
+                <option value="">-- none --</option>
+              </template>
+            </b-form-select>
+            <b-form-select
+              v-model="sortDesc"
+              size="sm"
+              :disabled="!sortBy"
+              class="w-25"
+            >
+              <option :value="false">Asc</option>
+              <option :value="true">Desc</option>
+            </b-form-select>
+          </b-input-group>
+        </b-form-group>
+      </b-col>
+      <b-col md="6" sm="6" class="my-1">
+        <b-form-group
+          label="Filter"
+          label-cols-sm="3"
+          label-align-sm="right"
+          label-size="sm"
+          label-for="filterInput"
+          class="mb-0"
+        >
+          <b-input-group size="sm">
+            <b-form-input
+              id="filterInput"
+              v-model="filter"
+              type="search"
+              placeholder="Type to Search"
+            />
+            <b-input-group-append>
+              <b-button :disabled="!filter" @click="filter = ''">
+                Clear
+              </b-button>
+            </b-input-group-append>
+          </b-input-group>
+        </b-form-group>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col class="table-responsive">
+        <b-table
+          striped
+          hover
+          responsive
+          :per-page="perPage"
+          :current-page="currentPage"
+          :items="items"
+          :fields="fields"
+          :sort-by.sync="sortBy"
+          :sort-desc.sync="sortDesc"
+          :sort-direction="sortDirection"
+          :filter="filter"
+          :filter-included-fields="filterOn"
+          @filtered="onFiltered"
+        >
+          <template #cell(is_active)="data">
+            <b-badge :variant="is_active[1][data.value]">
+              {{ is_active[0][data.value] }}
+            </b-badge>
+          </template>
+          
+          <template #cell(phone)="data">
+              <span class="text-info">
+                  {{ data.value }}
+              </span>
+          </template>
 
-    <b-col cols="12">
-      <b-table
-        striped
-        hover
-        responsive
-        :per-page="perPage"
-        :current-page="currentPage"
-        :items="items"
-        :fields="fields"
-        :sort-by.sync="sortBy"
-        :sort-desc.sync="sortDesc"
-        :sort-direction="sortDirection"
-        :filter="filter"
-        :filter-included-fields="filterOn"
-        @filtered="onFiltered"
-      >
-        <template #cell(is_active)="data">
-          <b-badge :variant="is_active[1][data.value]">
-            {{ is_active[0][data.value] }}
-          </b-badge>
-        </template>
-        
-        <template #cell(phone)="data">
-            <span class="text-info">
-                {{ data.value }}
-            </span>
-        </template>
-
-        <template #cell(email)="data">
-            <span class="text-primary">
-                <a href="#">{{ data.value }}</a>
-            </span>
-        </template>
-      </b-table>
-    </b-col>
-
-    <b-col cols="12">
-      <b-pagination
-        v-model="currentPage"
-        :total-rows="totalRows"
-        :per-page="perPage"
-        align="center"
-        size="sm"
-        class="my-0"
-      />
-    </b-col>
-  </b-row>
+          <template #cell(email)="data">
+              <span class="text-primary">
+                  <a href="#">{{ data.value }}</a>
+              </span>
+          </template>
+        </b-table>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col cols="12">
+          <b-pagination
+            v-model="currentPage"
+            :total-rows="totalRows"
+            :per-page="perPage"
+            align="center"
+            size="sm"
+            class="my-0"
+          />
+      </b-col>
+    </b-row>
+</div>
 </template>
 
 <script>
