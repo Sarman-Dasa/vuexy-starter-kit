9ae1f91d8f9068eb7cf954d7e6d8a86a9a22ac24
Sarman-Dasa <sarmand@zignuts.com>
2023-05-19 17:26:28 +0530

    create chart with static data

---
+++ src/views/Test/Chart.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <chartjs-doughnut
+        :labels="label"
+        :datasets="datasets"
+        :option="option"
+        >
+
+        </chartjs-doughnut>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                label:[
+                    'High',
+                    'Low',
+                    'Medium'
+                ],
+                datasets:[
+                    {
+                        data:[20,30,50],
+                        backgroundColor:[
+                            'Red',
+                            'Green',
+                            'Yellow'
+                        ]
+                    }
+                ],
+                option:{
+                    
+                    title:{
+                        display:true,
+                        postition:'center',
+                        text:'Todo'
+                    }
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
