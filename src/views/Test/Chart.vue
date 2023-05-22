90e68f95b345213c84a07231a4b17d5ac1718ef2
Sarman-Dasa <sarmand@zignuts.com>
2023-05-22 11:17:24 +0530

    get data from api and draw a chart

--- src/views/Test/Chart.vue
+++ src/views/Test/Chart.vue
@@ -1,47 +1,56 @@
 <template>
     <div>
-        <chartjs-doughnut
-        :labels="label"
-        :datasets="datasets"
+        <chartjs-doughnut 
+        :labels="label" 
+        :datasets="datasets" 
         :option="option"
+        :height="50"
         >
-
+          
         </chartjs-doughnut>
     </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                label:[
-                    'High',
-                    'Low',
-                    'Medium'
-                ],
-                datasets:[
-                    {
-                        data:[20,30,50],
-                        backgroundColor:[
-                            'Red',
-                            'Green',
-                            'Yellow'
-                        ]
-                    }
-                ],
-                option:{
-                    
-                    title:{
-                        display:true,
-                        postition:'center',
-                        text:'Todo'
-                    }
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            label: [
+                'High',
+                'Low',
+                'Medium'
+            ],
+            datasets: [
+                {
+                    data: [],
+                    backgroundColor: [
+                        'Red',
+                        'Green',
+                        'Yellow'
+                    ]
                 }
-            }
+            ],
+            option: {
+
+                title: {
+                    display: true,
+                    postition: 'center',
+                    text: 'Todo'
+                }
+            },
         }
+    },
+    async mounted() {
+        await axios.post("todo/list").then((success) => {
+            //console.log(success.data.data.priority)
+            let priority = success.data.data.priority;
+            Object.values(priority).forEach((val,index) => {
+                this.datasets[0].data[index] = val;
+            })
+        });
     }
+}
 </script>
 
-<style>
-
-</style>
+<style></style>
