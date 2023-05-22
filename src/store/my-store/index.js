17d06e5dfa554cb29f4f6a66ccf5d4dbb1da3866
Sarman-Dasa <sarmand@zignuts.com>
2023-05-22 18:18:13 +0530

    add diffrent type of getters

--- src/store/my-store/index.js
+++ src/store/my-store/index.js
@@ -2,7 +2,9 @@ export default {
     namespaced: true,
     state: {
         title:'vuex',
-        description:'state management'
+        description:'state management',
+        userId:'000',
+        phone:'965634235678'
     },
     mutations: {
         setNewTitle(state, payload) {
@@ -29,6 +31,21 @@ export default {
                     resolve('ok')
                    }, 2000)  
             })
-         }
+        }
+    },
+    getters: {
+        //get data with some modification 
+        hashAdd(state) {
+            return state.userId+ '####';
+        },
+        // call other getters
+        hashNewAdd(state,getters) {
+            return getters.hashAdd + " mobile Number "+ state.phone;
+        },
+        //pass data into getters
+        newUserId: (state) => (id) => {
+            return state.userId+''+id;
+        }
+
     }
 }
