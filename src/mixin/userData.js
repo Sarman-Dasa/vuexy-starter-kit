9c279cf3f9e5c9546cb5f853d948b6b89a5c55c5
Sarman-Dasa <sarmand@zignuts.com>
2023-05-26 17:00:07 +0530

    set user image path

--- src/mixin/userData.js
+++ src/mixin/userData.js
@@ -1,26 +1,30 @@
 import CryptoJS from 'crypto-js'
 export default {
     methods: {
-        loginUserData(token,userInfo) {
+        loginUserData(token='',userInfo='') {
             //set authToken in localstorage
+           if(token) {
             this.$store.commit('app/UPDATE_AUTH_TOKEN',token);
             localStorage.setItem('authTokenData',CryptoJS.AES.encrypt(token,process.env.VUE_APP_SECRET_KEY).toString());
+           }
 
             //set user info in localstorage
-            let user = {
-                id: userInfo.id,
-                email: userInfo.email,
-                first_name: userInfo.first_name,
-                is_active: userInfo.is_active,
-                last_name: userInfo.last_name,
-                phone: userInfo.phone,
-                role: userInfo.role.role
+            if(userInfo) {
+                let user = {
+                    id: userInfo.id,
+                    email: userInfo.email,
+                    first_name: userInfo.first_name,
+                    is_active: userInfo.is_active,
+                    last_name: userInfo.last_name,
+                    phone: userInfo.phone,
+                    avtar:userInfo.image,
+                    role: userInfo.role.role
+                }
+                this.$store.commit('app/UPDATE_LOGIN_USER_INFO',user);
+                userInfo = CryptoJS.AES.encrypt(JSON.stringify(user),process.env.VUE_APP_SECRET_KEY).toString();
+                localStorage.setItem('userInfoData',userInfo);
+                this.$router.push({name:'home'});
             }
-            
-            this.$store.commit('app/UPDATE_LOGIN_USER_INFO',user);
-            userInfo = CryptoJS.AES.encrypt(JSON.stringify(user),process.env.VUE_APP_SECRET_KEY).toString();
-            localStorage.setItem('userInfoData',userInfo);
-            this.$router.push({name:'home'});
         }
     }
 }
