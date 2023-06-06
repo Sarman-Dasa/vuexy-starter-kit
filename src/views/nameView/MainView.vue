26e5d0548bf0d0d11cd2c1e58dfdf0a4e3677f82
Sarman-Dasa <sarmand@zignuts.com>
2023-06-06 19:35:49 +0530

    route link for check route prop

---
+++ src/views/nameView/MainView.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <b-navbar toggleable="lg" type="dark" variant="info">
+            <b-navbar-brand href="#">
+                NAVBAR
+            </b-navbar-brand>
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav>
+                    <b-nav-item :to="{name:'second',params: { id: '1' }}">First</b-nav-item>
+                  
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+
+        <router-view class="view one"></router-view>
+        <router-view class="view two" name="a"></router-view>
+        <router-view class="view three" name="b"></router-view>
+    </div>
+</template>
+
+<script>
+import  {
+    BNavbar,
+    BNavbarBrand,
+    BNavbarNav,
+    BCollapse,
+    BNavbarToggle,
+    BNavItem
+} from 'bootstrap-vue';
+    export default {
+        components: {
+            BNavbar,
+            BNavbarBrand,
+            BNavbarNav,
+            BCollapse,
+            BNavbarToggle,
+            BNavItem
+        }
+    }
+</script>
+
